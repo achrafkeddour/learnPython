ce7853d3dd2176cc790ffdb926aa5a3cbdc8134c
achrafkeddour <152210432+achrafkeddour@users.noreply.github.com>
2025-04-09 15:47:29 +0100

    Create 2_escape_chars.py

---
+++ ezero/1-50/2_escape_chars.py
@@ -0,0 +1,22 @@
+# \b => back space 
+# \newline => Escape New Line + \
+# \' => Escape Single Quotes
+# \" => Escape Double Quotes
+# \r => Carriage Return
+# \xhh => Character Hex Value
+
+# back space
+print("hello\bpython") # hellpython
+
+# \newline
+print("hello \
+guys \
+how \
+are you") # hello guys how are you
+
+# single & double quotes 
+print("his name is \"ahmed\" ") # his name is "ahmed"
+print('his name is \'ahmed\' ') # his name is 'ahmed'
+
+# Carriage Return
+print("keddour\r1234") # 1234our  
